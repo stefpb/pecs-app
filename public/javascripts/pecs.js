5b0746295403b484bb73434a8041f4280f9e2648
Stefan Loehers <stef@loehers.de>
2012-05-07 03:34:03 +0200

    Added express.js to enable less-css

---
+++ public/javascripts/pecs.js
@@ -0,0 +1,75 @@
+var pool = {
+    'Foods & Drinks':{
+        'hot food':{
+            'items':[ 'pizza' ]
+        }
+    },
+    'Objects':{
+        'technical':{
+            'items':[ 'toaster' ]
+        },
+        'items':[ 'thing' ]
+    }
+};
+
+
+
+var selectedPath = [];
+var sentence = ['Can I'];
+
+
+function showPool() {
+    var selector = selectedPath;
+    var subPool = pool;
+    $('#pool').html('');
+    for(var i = 0; i<selector.length; i++)
+    {
+        subPool = subPool[selector[i]];
+    }
+
+    $.each(subPool, function(i) {
+        if(i !== 'items') {
+            $('<div class="category">' + i + '</div>').appendTo('#pool');
+        }
+    });
+
+    if(subPool.items) {
+        $.each(subPool.items, function() {
+            $('<div class="item">' + this + '</div>').appendTo('#pool');
+        });
+    }
+}
+
+function showSentence() {
+    $('#sentence').html('');
+    $.each(sentence, function() {
+        $('<div class="word">' + this + '</div>').appendTo('#sentence');
+    });
+    $('<div>+</div>').appendTo('#sentence').on('click', function() {
+        selectedPath = [];
+        showPool();
+    });
+}
+
+
+$(function() {
+
+    showSentence();
+
+    $('#pool div.category').live('click', function() {
+        selectedPath.push($(this).text());
+        showPool();
+    });
+
+    $('#pool .item').live('click', function() {
+        sentence.push($(this).text());
+        showSentence();
+    });
+
+    showPool([]);
+
+    $('#sentence .word').live('click', function() {
+        sentence.splice($(this).index(), 1);
+        showSentence();
+    });
+});
