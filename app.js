985cd0988d416dea03d4646adc1586f84a0eb565
Stef <edv@slws.de>
2012-05-24 17:24:48 +0000

    Port for deploy

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(process.env.C9_PORT || process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
